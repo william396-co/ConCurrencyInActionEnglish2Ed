7341f76b9b2845c51851e947e044b92870b0e9a9
刘会平 <will_369@outlook.com>
2026-03-04 17:47:34 +0800

    [feat] init chapter4 synchronizing concurrent operation

---
+++ Chapter4-SynchronizingConcurrentOperation/SychronizingConCurrentOperation.cpp
@@ -0,0 +1,13 @@
+#include "SychronizingConCurrentOperation.h"
+
+namespace waiting_for_an_event_or_other_condition {
+
+}
+
+void synchronzing_concurrent_operation_example() {
+	
+	{
+		using namespace waiting_for_an_event_or_other_condition;
+
+	}
+}
---
+++ Chapter4-SynchronizingConcurrentOperation/SychronizingConCurrentOperation.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+// 4.1 Waiting for an event or other condition
+namespace waiting_for_an_event_or_other_condition {
+
+}
+
+void synchronzing_concurrent_operation_example();
